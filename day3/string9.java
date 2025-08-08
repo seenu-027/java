19c0821419a92d71b8674e81ae07629c9d24f735
seenu-027 <149017596+seenu-027@users.noreply.github.com>
2025-08-09 04:22:54 +0530

    Create string9.java

---
+++ day3/string9.java
@@ -0,0 +1,12 @@
+package day3;
+public class string9 {
+
+	 public static void main(String[] args)
+	 {
+	 String str = "Java Exercises.";
+	 // Convert the above string to a char array.
+	 char[] arr = str.toCharArray();
+	 // Display the contents of the char array.
+	 System.out.println(arr);
+	 }
+	}
