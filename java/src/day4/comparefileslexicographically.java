1957bd1a5e6564194f3e32f1fa6d0f6185c9e912
seenu-027 <149017596+seenu-027@users.noreply.github.com>
2025-08-09 04:27:15 +0530

    Create comparefileslexicographically.java

---
+++ java/src/day4/comparefileslexicographically.java
@@ -0,0 +1,17 @@
+package day4;
+import java.io.File;
+public class comparefileslexicographically {
+   public static void main(String[] args) 
+   {
+       String str1 = "Java exercises";
+       String str2 = "Java exercises";
+       String str3 = "Java examples";
+
+       int var1 = str1.compareTo( str2 );
+       System.out.println("str1 & str2 comparison: "+var1);
+
+       int var2 = str1.compareTo( str3 );
+       System.out.println("str1 & str3 comparison: "+var2);
+
+    }
+}
