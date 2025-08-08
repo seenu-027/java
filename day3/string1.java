37cdd67ece92aeac5b635d453de3058ab85e3ff9
seenu-027 <149017596+seenu-027@users.noreply.github.com>
2025-08-09 04:19:38 +0530

    Create string1.java

---
+++ day3/string1.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class string1 {
+	 public static void main(String[] args)
+	 {
+	 String str = "Java Exercises!";
+	 System.out.println("Original String = " + str);
+	 // Get the character at positions 0 and 10.
+	 int index1 = str.charAt(0);
+	 int index2 = str.charAt(10);
+	 // Print out the results.
+	 System.out.println("The character at position 0 is " +
+	 (char)index1);
+	 System.out.println("The character at position 10 is " +
+	 (char)index2);
+	 }
+	}
