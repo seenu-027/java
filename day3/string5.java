efcd442c69381ed9c21f0622664b80717531cab8
seenu-027 <149017596+seenu-027@users.noreply.github.com>
2025-08-09 04:21:22 +0530

    Create string5.java

---
+++ day3/string5.java
@@ -0,0 +1,10 @@
+package day3;
+import java.util.Calendar;
+public class string5 {
+public static void main(String[] args) {
+ Calendar c = Calendar.getInstance();
+ System.out.println("Current Date and Time :");
+ System.out.format("%tB %te, %tY%n", c, c, c);
+ System.out.format("%tl:%tM %tp%n", c, c, c);
+ }
+ }
