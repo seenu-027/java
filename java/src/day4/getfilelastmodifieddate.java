ee59656453809be922f577538aaf205b8fd2710a
seenu-027 <149017596+seenu-027@users.noreply.github.com>
2025-08-09 04:28:41 +0530

    Create getfilelastmodifieddate.java

---
+++ java/src/day4/getfilelastmodifieddate.java
@@ -0,0 +1,10 @@
+package day4;
+import java.io.File;
+import java.util.Date;
+public class getfilelastmodifieddate {
+       public static void main(String[] args) {
+       File file = new File("test.txt");
+       Date date=new Date(file.lastModified());
+	   System.out.println("\nThe file was last modified on: "+date+"\n");	   
+     }
+}
