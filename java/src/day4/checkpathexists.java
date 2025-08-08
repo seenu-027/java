2c6eb4ac08d600d20c7639b241994d4d74a53ced
seenu-027 <149017596+seenu-027@users.noreply.github.com>
2025-08-09 04:25:44 +0530

    Create checkpathexists.java

---
+++ java/src/day4/checkpathexists.java
@@ -0,0 +1,16 @@
+package day4;
+import java.io.File;
+public class checkpathexists {
+	       public static void main(String[] args) {
+	        // Create a File object
+	        File my_file_dir = new File("/home/students/xyz.txt");
+	         if (my_file_dir.exists()) 
+	           {
+	            System.out.println("The directory or file exists.\n");
+	           } 
+	         else
+	          {
+	            System.out.println("The directory or file does not exist.\n");
+	          }
+	       }
+	  }
