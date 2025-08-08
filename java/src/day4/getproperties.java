a4cf6734b9cefe825ffa5a5e4e2ee8c657793008
seenu-027 <149017596+seenu-027@users.noreply.github.com>
2025-08-09 04:29:26 +0530

    Create getproperties.java

---
+++ java/src/day4/getproperties.java
@@ -0,0 +1,8 @@
+package day4;
+import java.util.*;
+public class getproperties {
+	public static void main (String [] args) {
+	Properties x = System. getProperties () ;
+	x. list (System.out);
+	}
+	}
